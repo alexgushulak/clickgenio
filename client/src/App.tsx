19b8720578395c681c24812f66b9e87c71cb2b7a
Parijat Tripathi <parijat.tripathi@siemens.com>
2023-09-05 21:48:12 -0500

    refactor: move api calls to service layer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,89 +1,30 @@
-import Container from '@mui/material/Container';
-import { Button, Typography } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
-import axios from 'axios';
+// App.tsx
 import React, { useState } from 'react';
-import TextField from '@mui/material/TextField';
+import { CssBaseline, Container, Typography, TextField, Button, Box } from '@mui/material';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { submitData, generateImage } from './services/apiLayer';
 import RainbowTesla from './assets/rainbow_tesla.png'
 import Tsunami from './assets/tsunami.png'
-import Box from '@mui/material/Box';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Routes,
-  Route,
-  useNavigate,
-} from "react-router-dom";
 
 export default function App() {
-  const navigate = useNavigate();
   const engineId = 'stable-diffusion-xl-1024-v1-0'
   const apiHost = 'https://api.stability.ai'
-  const apiKey = "sk-36jFn0ywSl2ktMvPnqdMdcbJRdI1x3bNLL8Hydd81XrmxWT9" // THIS NEEDS TO BE MOVED TO THE SERVER
-
+  const apiKey = "sk-36jFn0ywSl2ktMvPnqdMdcbJRdI1x3bNLL8Hydd81XrmxWT9"
   const [thumbnailText, setThumbnailText] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-
-  /** Input is one base64 encoded image
-   *  Returns the image decoded in jpeg format"
-   * */
-  const decodeImage = (image_object: any) => {
-    const base64_decoded_image = atob(image_object.artifacts[0].base64)
-    const byteNumbers = new Array(base64_decoded_image.length);
-    for (let i = 0; i < base64_decoded_image.length; i++) {
-        byteNumbers[i] = base64_decoded_image.charCodeAt(i);
-    }
-    const byteArray = new Uint8Array(byteNumbers);
-
-    let decoded_jpeg_image = new Blob([byteArray], { type: 'image/jpeg' });
-    return decoded_jpeg_image
-  }
-
-  /** tracks clicks on the generate thumbnail button" */
-  const trackClick = () => {
-    axios.post('https://alex-portfolio-production.up.railway.app/submit',
-              {message: thumbnailText}
-    ).then((res) => {
-      // do nothing
-    });
-
-    const image = axios.post(`${apiHost}/v1/generation/${engineId}/text-to-image`, {
-        text_prompts: [
-            {
-                text: `${thumbnailText}`,
-            },
-            ],
-            cfg_scale: 7,
-            height: 832,
-            width: 1216,
-            steps: 10,
-            samples: 1,
-        
-    }, {
-        headers: {
-            'Content-Type': 'application/json',
-            Accept: 'application/json',
-            Authorization: `Bearer ${apiKey}`,
-        }
-    }).then((response) => {
-        let image = decodeImage(response.data)
-        setImageUrl(URL.createObjectURL(image))
-    }).catch((error) => {
-        console.log(error)
-    })
-
-    console.log("Ending Post")
-
-    navigate("/submit")
-  }
+  const navigate = useNavigate();
 
   const handleTextbarChange = (event: { target: { name: any; value: any; }; }) => {
-    const { name, value } = event.target
-    setThumbnailText(value)
-  }
+    const { name, value } = event.target;
+    setThumbnailText(value);
+  };
 
-  React.useEffect(() => {
-  }, []);
+  const trackClick = async () => {
+    await submitData(thumbnailText);
+    await generateImage(thumbnailText, apiHost, engineId, apiKey, setImageUrl);
+    console.log("Ending Post");
+    navigate("/submit");
+  };
 
   return (
     <div>
@@ -94,7 +35,7 @@ export default function App() {
         </Routes>
           <Container sx={{ mb: 5, mt: 15 }}>
           <img src={imageUrl} />
-          <Typography 
+          <Typography
             variant="h3"
             component="h3"
             sx={{ display: 'block', 'text-align': 'center', margin: '0 auto', mt: 1}}>
