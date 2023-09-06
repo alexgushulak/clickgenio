59589215674aed810deaa8bff6f01de8c2d222c4
alexgushulak <alex.gushulak@gmail.com>
2023-09-06 12:53:51 -0400

    Move stability.ai api call to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const res = require('express/lib/response')
 const cors = require('cors')
 const geoip = require('geoip-lite')
+const axios = require('axios')
 const app = express()
 var bodyParser = require('body-parser')
 const port = 3000
@@ -10,6 +11,46 @@ const port = 3000
 var jsonParser = bodyParser.json()
 
 app.use(cors())
+
+const decodeImage = (image_object) => {
+    const base64_decoded_image = atob(image_object.artifacts[0].base64)
+      const byteNumbers = new Array(base64_decoded_image.length);
+      for (let i = 0; i < base64_decoded_image.length; i++) {
+          byteNumbers[i] = base64_decoded_image.charCodeAt(i);
+      }
+      const byteArray = new Uint8Array(byteNumbers);
+
+      let decoded_jpeg_image = new Blob([byteArray], { type: 'image/jpeg' });
+      return decoded_jpeg_image
+};
+
+const generateImage = async (thumbnailText, apiHost, engineId, apiKey) => {
+    try {
+      const response = await axios.post(`${apiHost}/v1/generation/${engineId}/text-to-image`, {
+        text_prompts: [
+          {
+            text: thumbnailText,
+          },
+        ],
+        cfg_scale: 7,
+        height: 832,
+        width: 1216,
+        steps: 10,
+        samples: 1,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${apiKey}`,
+        }
+      });
+
+      console.log("Generate Image Successful");
+      return response.data;
+    } catch (error) {
+      console.error("Generate Image Error:", error);
+    }
+  };
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -28,10 +69,14 @@ app.get('/info', (req, res) => {
     })
 })
 
-app.post('/submit', jsonParser, (req,res) => {
-    console.log(`Message Recieved: ${req.body.message}`)
+app.post('/submit', jsonParser, async (req,res) => {
+    const thumbnail_image_text = req.body.message
+    const engineId = "stable-diffusion-xl-1024-v1-0";
+    const apiHost = "https://api.stability.ai";
+    const apiKey = "sk-36jFn0ywSl2ktMvPnqdMdcbJRdI1x3bNLL8Hydd81XrmxWT9";
+    const response = await generateImage(thumbnail_image_text, apiHost, engineId, apiKey)
     res.status(200).send({
-        message: "Data received"
+        message: response
     })
 })
 
