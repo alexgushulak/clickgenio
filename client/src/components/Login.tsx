7dad5e0d08b6c08ba51b91f87cf7f7c530e03913
Alex Gushulak <alex.gushulak@gmail.com>
2023-10-18 20:20:03 -0400

    Initial Commit

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -10,13 +10,17 @@ import Menu from '@mui/material/Menu';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import Badge from '@mui/material/Badge';
+import TollIcon from '@mui/icons-material/Toll';
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from '../main';
+import { getCredits } from '../services/apiLayer';
 
 
 export default function Login() {
     const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+    const [credits, setCredits] = React.useState<null | number>(null);
     const [cookies, setCookie, removeCookie] = useCookies(['token', 'given_name', 'pictureURL', 'credits']);
     const authContext = useContext(AuthContext);
 
@@ -45,9 +49,11 @@ export default function Login() {
                     code: codeResponse.code,
                 });
 
+            console.log(login_info)
+
             setCookie('token', login_info.data.id_token)
-            setCookie('given_name', login_info.data.payload.given_name)
-            setCookie('pictureURL', login_info.data.payload.picture)
+            setCookie('given_name', login_info.data.given_name)
+            setCookie('pictureURL', login_info.data.picture)
 
             authContext?.setIsLoggedIn(true)
         },
@@ -62,10 +68,30 @@ export default function Login() {
         navigate('/')
     }
 
-    useEffect(() => {
-        if (cookies.token) {
-            authContext?.setIsLoggedIn(true)
+    const buyTokens = () => {
+        if (authContext?.isLoggedIn == true) {
+            navigate('/purchase')
+        } else {
+            googleLogin()
         }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (cookies.token) {
+                authContext?.setIsLoggedIn(true);
+            }
+        
+            try {
+                const response = await getCredits(cookies.token);
+                setCredits(response.credits);
+            } catch (error) {
+                // Handle any errors that occur while fetching credits.
+                console.error('Error fetching credits:', error);
+            }
+        };
+
+        fetchData()
     }, [setCookie, removeCookie])
       
     
@@ -82,9 +108,12 @@ export default function Login() {
             >
                 Sign in
             </Button>
-            <Box sx={{ display: authContext?.isLoggedIn ? 'flex' : 'none' ,flexGrow: 0 }}>
-                <Button variant="contained" sx={{display: 'none', marginRight: '20px'}}>
-                    Buy Tokens
+            <Box sx={{ display: authContext?.isLoggedIn ? 'flex' : 'none'}}>
+                <Button variant="outlined" sx={{cursor: 'default', display: authContext?.isLoggedIn ? 'flex' : 'none', marginRight: '20px'}}>
+                    Credits: {credits}
+                </Button>
+                <Button variant="contained" onClick={buyTokens} sx={{display: authContext?.isLoggedIn ? 'flex' : 'none', marginRight: '20px'}}>
+                    Buy Credits
                 </Button>
                 <Tooltip title="">
                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
@@ -107,7 +136,10 @@ export default function Login() {
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
                 >
-                <MenuItem key="1" onClick={logout}>
+                <MenuItem key="1">
+                    <Typography textAlign="center">Support: clickgenio11@gmail.com</Typography>
+                </MenuItem>
+                <MenuItem key="2" onClick={logout}>
                     <Typography textAlign="center">Logout</Typography>
                 </MenuItem>
                 </Menu>
--- client/src/services/apiLayer.tsx
+++ client/src/services/apiLayer.tsx
@@ -133,5 +133,34 @@ export const updateIsEmailOk = async (token: string) => {
   }
 }
 
+export const getCredits = async (token: string) => {
+  try {
+    console.log(token)
+    const response = await axios.get(`${import.meta.env.VITE_APISERVER}/user/get-credits?token=${token}`);
+    return response.data
+  } catch (error) {
+    console.error('getCredits Error', error);
+    return 403;
+  }
+}
 
-export { generateImage, submitIPData, downloadImage, submitThumbnailData, submitDownloadData, submitBuyData, updateImageData };
+export const createCreditCheckoutSession = async (credits: number, email: string) => {
+  try {
+    const response = await axios.post(`${import.meta.env.VITE_APISERVER}/create-checkout-session?credits=${credits}&email=${email}`);
+    return response
+  } catch (error) {
+    console.error('Update isEmailOk Error', error);
+    return 403;
+  }
+}
+
+
+export {
+  generateImage,
+  submitIPData,
+  downloadImage,
+  submitThumbnailData,
+  submitDownloadData,
+  submitBuyData,
+  updateImageData
+};
