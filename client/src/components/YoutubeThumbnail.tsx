1e588c64e07660e3421ddc6849a9052a451cee6b
Alex Gushulak <alex.gushulak@gmail.com>
2023-10-19 23:23:09 -0400

    (fix) limit length of title in yt thumbnails

--- client/src/components/YoutubeThumbnail.tsx
+++ client/src/components/YoutubeThumbnail.tsx
@@ -29,6 +29,8 @@ interface YouTubeThumbnailProps {
 }
 
 const YouTubeThumbnail: React.FC<YouTubeThumbnailProps> = ({ imageSrc, title, viewCount }) => {
+
+
   return (
     <Grid sx={{width: '100%', padding: '10px 0px', margin: '5px', height: {xs: '100%', sm: '160px'}}}>
         <Grid item xs={12} sm={7} sx={{display: 'inline-block', float: 'left'}}>
@@ -45,7 +47,7 @@ const YouTubeThumbnail: React.FC<YouTubeThumbnailProps> = ({ imageSrc, title, vi
           textAlign: 'left',
           float: 'left',
           }}>
-            <Typography sx={{fontSize: '15px', padding: '0px 10px'}}><strong>{capitalize(title)}</strong></Typography>
+            <Typography sx={{fontSize: '15px', padding: '0px 10px'}}><strong>{capitalize(title).substring(0,150)}</strong></Typography>
             <Typography sx={{fontSize: '14px', padding: '2px 0px 2px 10px', display: 'inline-block', float: 'left'}}>ClICKGEN.IO</Typography><CheckCircleIcon sx={{display: 'inline-block', right: '10px', height: '18px', paddingTop: '5px'}}/>
             <Typography sx={{padding: '2px 10px'}}>{viewCount}M Views</Typography>
         </Grid>
